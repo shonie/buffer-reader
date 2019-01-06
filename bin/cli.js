1455ff30e8f4ec84aba8dface03f31b0292987c1
shonie <astarnikov@thesolid.studio>
2019-01-06 16:05:43 +0200

    Works!

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,46 @@
 #!/usr/bin/env node
 
-const bufferized = require('../lib');
+const readline = require('readline');
+const { width } = require('window-size');
+const { composeLines } = require('../lib/layout');
 
 async function main() {
-  for await (const line of bufferized(process.stdin.setEncoding('utf8'))) {
-    // process.stdout.write(line);
-    console.log(line);
-  }
+  const rl = readline.createInterface({
+    input: process.stdin,
+  });
+
+  let appendix = '';
+
+  const columnWidth = width / 2;
+
+  // Actual width is smaller than half, as we append '|' as column separator
+  const columnInnerWidth = columnWidth - 2;
+
+  rl.on('line', line => {
+    for (const raw of composeLines(line, columnInnerWidth)) {
+      const utf8 = raw.padEnd(columnInnerWidth);
+
+      let hex = `${appendix}${Buffer.from(`${line}\n`, 'utf8').toString('hex')}`;
+
+      if (hex.length > columnInnerWidth) {
+        appendix = hex.slice(columnInnerWidth);
+        hex = hex.slice(0, columnInnerWidth);
+      } else {
+        appendix = '';
+      }
+
+      console.log(utf8, '|', hex);
+    }
+  });
+
+  rl.on('close', () => {
+    // console.log('appendix', appendix)
+    if (appendix) {
+      for (const raw of composeLines(appendix, columnInnerWidth)) {
+        console.log(''.padEnd(columnInnerWidth), '|', raw);
+      }
+    }
+  });
 }
 
 main();
