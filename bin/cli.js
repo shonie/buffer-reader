00ab151b77b18350f8e80434f69855f065117de8
shonie <astarnikov@thesolid.studio>
2019-01-07 14:25:16 +0200

    Refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -1,46 +1,22 @@
 #!/usr/bin/env node
 
 const readline = require('readline');
-const { width } = require('window-size');
-const { composeLines } = require('../lib/layout');
+const { width: terminalWidth } = require('window-size');
+const { makeRows } = require('../lib/layout');
 
 async function main() {
-  const rl = readline.createInterface({
+  const reader = readline.createInterface({
     input: process.stdin,
   });
 
-  let appendix = '';
-
-  const columnWidth = width / 2;
-
-  // Actual width is smaller than half, as we append '|' as column separator
-  const columnInnerWidth = columnWidth - 2;
-
-  rl.on('line', line => {
-    for (const raw of composeLines(line, columnInnerWidth)) {
-      const utf8 = raw.padEnd(columnInnerWidth);
-
-      let hex = `${appendix}${Buffer.from(`${line}\n`, 'utf8').toString('hex')}`;
-
-      if (hex.length > columnInnerWidth) {
-        appendix = hex.slice(columnInnerWidth);
-        hex = hex.slice(0, columnInnerWidth);
-      } else {
-        appendix = '';
-      }
-
-      console.log(utf8, '|', hex);
-    }
+  const rowsGenerator = makeRows(reader, {
+    separator: '|',
+    columnWidth: terminalWidth / 2 - 2,
   });
 
-  rl.on('close', () => {
-    // console.log('appendix', appendix)
-    if (appendix) {
-      for (const raw of composeLines(appendix, columnInnerWidth)) {
-        console.log(''.padEnd(columnInnerWidth), '|', raw);
-      }
-    }
-  });
+  for await (const row of rowsGenerator) {
+    console.log(row);
+  }
 }
 
 main();
